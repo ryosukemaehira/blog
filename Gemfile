28b43fcaf837b77fb6703be8877374c39d4ba720
maehira <wwwandering.jp@gmail.com>
2016-10-22 04:28:21 +0900

    basic auth

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'pry-rails'
 gem 'erb2haml'
 gem 'haml-rails'
 gem 'qiita-markdown'
+gem 'dotenv-rails'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
+
+  before_filter :basic_auth, only: [:new]
   def index
   end
 
   def new
+  def basic_auth
+    authenticate_or_request_with_http_basic do |user,pass|
+      user == ENV["BASIC_AUTH_USER"] && ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
