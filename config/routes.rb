402df10f31f87077a9af3b16451b1d08a9af304d
maehira <wwwandering.jp@gmail.com>
2016-10-23 20:39:54 +0900

    add function works#index and show

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'articles#index'
   resources :articles, only: [:new, :create, :show]
   resources :categories, only: [:index, :show]
+  resources :works, only: [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023034624) do
+ActiveRecord::Schema.define(version: 20161023101439) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20161023034624) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.integer  "articles_count", limit: 4
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.text     "detail",      limit: 65535
+    t.text     "image",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
