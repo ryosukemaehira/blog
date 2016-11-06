21029ee3d63141283fd9e341b34f3a8d42d2fb57
maehira <wwwandering.jp@gmail.com>
2016-11-07 03:58:09 +0900

    new actions and controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,24 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def new
+    @work = Work.new
+    @work.work_images.build
+  end
+
+  def create
+    @work = Work.create(create_params)
+    redirect_to :root
+  end
+
+  def show
+    @work = Work.find(params[:id])
+  end
+
+  private
+
+  def create_params
+    params.require(:work).permit(:title, :description, :detail, work_images_attributes: [:id, :content, :state])
+  end
 end
