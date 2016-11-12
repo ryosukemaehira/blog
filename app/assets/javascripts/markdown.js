c3b9abff604161a33ad3474c804c38a8e3653f51
maehira <wwwandering.jp@gmail.com>
2016-11-12 20:08:41 +0900

    js to drop image

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -1,9 +1,25 @@
+saveImage = function() {
+}
+
 $(document).on('turbolinks:load', function(){
   $(function(){
     $('#js-markdown-input').keyup(function(){
       var md = $(this).val();
       var html = marked(md);
       $('#js-markdown-preview').html(html);
+      console.log('そもそもkeyupは呼ばれてる？');
+
+      $('#js-markdown-input').on("drop", function(_e){
+        var e = _e;
+        if( _e.originalEvent ){
+            e = _e.originalEvent;
+        }
+        e.preventDefault();
+        e.stopPropagation();
+        var imageData = e.dataTransfer
+        console.log(e.dataTransfer);
+        saveImage;
+      });
     });
   });
-})
+});
